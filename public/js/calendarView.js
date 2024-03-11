074f6ede1f6b6d5b025da4b5ede7e571158e3f18
Ben Balls <b3njamin.balls@gmail.com>
2024-03-11 14:09:58 +0000

    sorted out first and last day of month

--- public/js/calendarView.js
+++ public/js/calendarView.js
@@ -1,13 +1,62 @@
-let monthSelected = 'May'
-let desiredFirstOfMonth
+let month //goes from 1-12
+let year
+let desiredFirstOfMonth //goes from 1 - 7
+let daysInMonth
 let count = 1
 const months = {
     jan: 'Jan'
 }
 
-let getFirstDayOfMonth = function (month, year) {
+let getFirstDayOfMonth = function () {
     let day = new Date(`${year}-${month}-1`);
-    return day
+    desiredFirstOfMonth = day.getDay()
 }
 
-desiredFirstOfMonth = getFirstDayOfMonth(3, 2024)
+let getDaysInMonth = function () {
+    let lastDay = new Date(year, month, 0);
+    daysInMonth = lastDay.getDate()
+}
+
+let setDays = function () {
+    getFirstDayOfMonth()
+    getDaysInMonth()
+    let date = 1
+    let dayEls = document.querySelectorAll('.day');
+    for (let i = 1; i <= dayEls.length - 1; i++) {
+        if (i < desiredFirstOfMonth) {
+            continue
+        }
+        if (date > daysInMonth) {
+            break
+        }
+        dayEls[i - 1].innerHTML = date
+        date++;
+    }
+}
+
+let clearDays = function () {
+    let dayEls = document.querySelectorAll('.day');
+    for (let day of dayEls) {
+        day.innerHTML = ''
+    }
+}
+
+let selectedMonth = document.querySelector('#month-picker')
+selectedMonth.addEventListener('change', function (e) {
+    let selectedMonthSplit = selectedMonth.value.split('-')
+    year = parseInt(selectedMonthSplit[0])
+    month = parseInt(selectedMonthSplit[1])
+    clearDays()
+    setDays()
+})
+
+let setup = function () {
+    let today = new Date()
+    month = today.getMonth() + 1
+    year = today.getFullYear()
+    setDays()
+}
+
+
+
+setup()
